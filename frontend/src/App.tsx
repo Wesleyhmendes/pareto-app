a5018056b0be117c114106b42611e55c2e263642
Wesley Mendes <wesleymendes123321@gmail.com>
2024-04-29 20:21:22 -0300

    Feat - add google social login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Messages from './pages/Messages';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route path="/" element={ <Login /> } />
+      <Route path="/chats" element={ <Home /> } />
       <Route path="/chat/:id" element={ <Messages /> } />
     </Routes>
   );
